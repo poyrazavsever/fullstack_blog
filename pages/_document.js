3d76591b126b952ac12b7f2dc2751f8f015448fc
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-08 10:19:43 +0300

    feat: added new container

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+
   return (
-    <Html lang="en">
+    <Html lang="en" className="dark">
       <Head />
       <body className="bg-bg_light dark:bg-bg_dark">
         <Main />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors:{
         bg_dark:"#14181D",
         bg_light:"#fffff"
-      }
+      },
+      container: {
+        center: true, // Container'ı ortalar
+        padding: '1rem', // Container'a padding ekler
+        screens: {
+          sm: '100%', // Küçük ekranlar için tam genişlik
+          md: '640px', // Orta boyutlu ekranlar için genişlik
+          lg: '768px', // Büyük ekranlar için genişlik
+          xl: '1024px', // Extra büyük ekranlar için genişlik
+        },
+      },
     },
   },
   plugins: [],
