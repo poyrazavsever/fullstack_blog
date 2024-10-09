63da008669c65b4cf35e5db8b48d103c54499699
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-09 23:37:32 +0300

    feat:created footer

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="w-full text-center py-12">
+      <p className="text-lg font-medium text-neutral-900 dark:text-neutral-200">Created By <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r dark:from-neutral-200 dark:to-amber-700 from-stone-800 to-amber-700">Poyraz Avsever</span></p>
+    </div>
   )
 }
 
