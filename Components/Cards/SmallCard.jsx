d68b9136667a989cef0ad718f31a9407ff466203
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-10 20:08:19 +0300

    feat: added simple detail link

--- Components/Cards/SmallCard.jsx
+++ Components/Cards/SmallCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SmallCard = () => {
     return (
-        <a href={`/`} target='_blank'>
+        <a href={`/blog/1`}>
 
             {/* Framer Motion'ı kullanarak hover animasyonu */}
             <div className='group relative border border-neutral-800 bg-opacity-20 bg-neutral-900 rounded-lg'>
