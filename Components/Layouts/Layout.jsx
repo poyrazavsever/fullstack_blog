e5c8caad72084fa82f3f287e936f5261cb2b77d5
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-10 21:36:20 +0300

    feat: added div with blur for background

--- Components/Layouts/Layout.jsx
+++ Components/Layouts/Layout.jsx
@@ -10,6 +10,8 @@ const Layout = ({children}) => {
         {children}
 
         <Footer />
+
+        <div className='absolute w-48 h-48 top-1/2 left-1/2 bg-amber-600 rounded-full blur-[140px] -z-50'/>
     </>
   )
 }
