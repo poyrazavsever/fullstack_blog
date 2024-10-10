9ebe6c190134caad5f6c65c1e456b09d860b4b57
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-10 21:44:40 +0300

    fix: card bg-opacity problem

--- Components/Cards/InfoCard.jsx
+++ Components/Cards/InfoCard.jsx
@@ -1,6 +1,6 @@
 const InfoCard = ({ title, description, icon }) => {
     return (
-        <div className="p-6 bg-white border border-neutral-300 dark:bg-neutral-800 dark:border-neutral-700 bg-opacity-5 backdrop-blur-md rounded-lg shadow-lg text-white">
+        <div className="p-6 bg-white border border-neutral-300 dark:bg-neutral-800 dark:border-neutral-700 bg-opacity-10 dark:bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg text-white">
             <div className="flex items-center space-x-3">
                 <div className="text-2xl">{icon}</div>
                 <h3 className="text-xl font-semibold">{title}</h3>
