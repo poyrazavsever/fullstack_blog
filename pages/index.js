77dd133daf546059c006fcbc7864f19dcca8489d
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-10 09:04:23 +0300

    feat: done cards

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
+import LargeCard from "@/Components/Cards/LargeCard";
+
 export default function Home() {
   return (
-    <div></div>
+    <div className="container mx-auto my-24">
+      <LargeCard />
+    </div>
   );
 }
