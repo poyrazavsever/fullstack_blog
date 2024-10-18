fc04e768b79e21371e34c969427ac643790e9396
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-18 12:08:10 +0300

    fix: data.data

--- features/category/thunk/fetchCategories.js
+++ features/category/thunk/fetchCategories.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 
 export const fetchCategories = createAsyncThunk('categories/fetch', async () => {
     const response = await axios.get('http://localhost:5000/api/categories/all');
-    return response.data;
+    return response.data.data;
 });
