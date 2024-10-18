0880bd4cddd488819a49610d042c70785c3239cb
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-18 08:44:51 +0300

    feat: created updatePost thunk

---
+++ features/post/thunks/getLastPost.js
@@ -0,0 +1,7 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const getLastPost = createAsyncThunk('posts/getLastPost', async () => {
+  const response = await axios.get('http://localhost:5000/api/posts/latest');
+  return response.data.data;
+});
