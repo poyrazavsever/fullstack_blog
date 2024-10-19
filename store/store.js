896c1a69768d6f0066d4cddcb614053499e882b3
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-19 11:00:29 +0300

    feat: added authReducer to store

--- store/store.js
+++ store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from "../features/post/postSlice"
 import categoriesReducer from "../features/category/categorySlice"
+import authReducer from '../features/auth/authSlice';
 
 const store = configureStore({
   reducer: {
     posts: postsReducer,
-    categories: categoriesReducer
+    categories: categoriesReducer,
+    auth: authReducer,
   },
 });
 
